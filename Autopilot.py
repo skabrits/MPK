b3c696943d66a99dc1602ded9a96d3677a08236d
sevakabrits <seva.kabrits@gmail.com>
2022-05-18 14:01:28 +0300

    MPK 0.0.2 alpha

--- Autopilot.py
+++ Autopilot.py
@@ -1,9 +1,23 @@
 import yaml
 import os
+import numpy as np
+
 
 class Uav:
-    def __init__(self, ):
-        pass
+    def __init__(self, r, surface_distance, angle, v_earth, v_air, rpm, current, voltage, temperature, *args, **kwargs):
+        self.r = np.array(r) if len(np.array(r)) != 0 else np.array(r).reshape(1,)
+        self.surface_distance = np.array(surface_distance) if len(np.array(surface_distance)) != 0 else np.array(surface_distance).reshape(1,)
+        self.angle = np.array(angle) if len(np.array(angle)) != 0 else np.array(angle).reshape(1,)
+        self.v_earth = np.array(v_earth) if len(np.array(v_earth)) != 0 else np.array(v_earth).reshape(1,)
+        self.v_air = np.array(v_air) if len(np.array(v_air)) != 0 else np.array(v_air).reshape(1,)
+        self.rpm = np.array(rpm) if len(np.array(rpm)) != 0 else np.array(rpm).reshape(1,)
+        self.current = np.array(current) if len(np.array(current)) != 0 else np.array(current).reshape(1,)
+        self.voltage = np.array(args) if len(np.array(args)) != 0 else np.array(args).reshape(1,)
+        self.temperature = np.array(args) if len(np.array(args)) != 0 else np.array(args).reshape(1,)
+        self.args = np.array(args) if len(np.array(args)) != 0 else np.array(args).reshape(1,)
+        for i in kwargs.keys():
+            exec("self." + str(i) + " = np.array(kwargs[i]) if len(np.array(kwargs[i])) != 0 else np.array(kwargs[i]).reshape(1,)")
+
 
 class AutopilotModule:
     def __init__(self, output_params_num, type="tiltwing", scheme_path="./scheme.yaml"):
--- simulation/autopilotf.py
+++ simulation/autopilotf.py
@@ -1,11 +1,5 @@
 
 
-def autopilot(r, surface_distance, v_earth, v_air, current):
-    '''
-    :param r:
-    :param surface_distance:
-    :param v_earth:
-    :param v_air:
-    :param current:
-    :return:
-    '''
+def autopilot(r, surface_distance, angles, v_earth, v_air, rpm, current, voltage, temperatures):
+
+    pass
